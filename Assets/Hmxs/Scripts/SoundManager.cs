1144c0281f298bbbc22ba31791f0eacc9f3ec5d6
hmxs <hmxsqaq@163.com>
2024-04-01 20:27:40 +0800

    Update NPC Sound

---
+++ Assets/Hmxs/Scripts/SoundManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Hmxs.Scripts
+{
+    public class SoundManager : MonoBehaviour
+    {
+        public static SoundManager Instance;
+
+        private void Awake()
+        {
+            if (Instance == null)
+                Instance = this;
+            else
+                Destroy(gameObject);
+        }
+
+        // [ListDrawerSettings(ShowIndexLabels = true)] [ReadOnly]
+        // [SerializeField] private List<string> info = new()
+        // {
+        //     "人-暴躁-女-东北话",
+        //     "狗-暴躁-女-东北话",
+        //     "人-温柔-女-普通话",
+        //     "狗-温柔-女-普通话"
+        // };
+
+        [InfoBox("" +
+                 "0: 人-暴躁-女-东北话\n" +
+                 "1: 狗-暴躁-女-东北话\n" +
+                 "2: 人-温柔-女-普通话\n" +
+                 "3: 狗-温柔-女-普通话\n" +
+                 "4: 人-暴躁-男-otto\n" +
+                 "5: 狗-暴躁-男-otto\n" +
+                 "6: 人-阴阳-男-东北话\n" +
+                 "7: 狗-阴阳-男-东北话\n" +
+                 "8: 人-暴躁-男-孙笑川\n" +
+                 "9: 狗-暴躁-男-孙笑川")]
+
+        [SerializeField] private List<SoundGroup> audioClips = new();
+
+        public void PlaySound(int id, Transform source = null)
+        {
+            var playPoint = source == null ? Vector3.zero : source.position;
+            AudioSource.PlayClipAtPoint(audioClips[id].clips[Random.Range(0, audioClips[id].clips.Count)], playPoint);
+        }
+
+        [Serializable]
+        public class SoundGroup
+        {
+            public List<AudioClip> clips = new();
+        }
+    }
+}
---
+++ Assets/Hmxs/Scripts/Test.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+namespace Hmxs.Scripts
+{
+    public class Test : MonoBehaviour
+    {
+        private void Update()
+        {
+            if (UnityEngine.Input.GetKeyDown(KeyCode.Alpha0))
+            {
+                SoundManager.Instance.PlaySound(0);
+            }
+            if (UnityEngine.Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                SoundManager.Instance.PlaySound(1);
+            }
+            if (UnityEngine.Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                SoundManager.Instance.PlaySound(2);
+            }
+            if (UnityEngine.Input.GetKeyDown(KeyCode.Alpha3))
+            {
+                SoundManager.Instance.PlaySound(3);
+            }
+            if (UnityEngine.Input.GetKeyDown(KeyCode.Alpha4))
+            {
+                SoundManager.Instance.PlaySound(4);
+            }
+            if (UnityEngine.Input.GetKeyDown(KeyCode.Alpha5))
+            {
+                SoundManager.Instance.PlaySound(5);
+            }
+            if (UnityEngine.Input.GetKeyDown(KeyCode.Alpha6))
+            {
+                SoundManager.Instance.PlaySound(6);
+            }
+            if (UnityEngine.Input.GetKeyDown(KeyCode.Alpha7))
+            {
+                SoundManager.Instance.PlaySound(7);
+            }
+            if (UnityEngine.Input.GetKeyDown(KeyCode.Alpha8))
+            {
+                SoundManager.Instance.PlaySound(8);
+            }
+            if (UnityEngine.Input.GetKeyDown(KeyCode.Alpha9))
+            {
+                SoundManager.Instance.PlaySound(9);
+            }
+        }
+    }
+}

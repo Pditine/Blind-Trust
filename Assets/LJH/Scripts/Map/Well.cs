2e94a9135f57f776579f55cc738fd1f09e9de0fa
Pditine <hanfe666@outlook.com>
2024-04-02 21:17:42 +0800

    场景修改,井,行人

---
+++ Assets/LJH/Scripts/Map/Well.cs
@@ -0,0 +1,32 @@
+﻿using Hmxs.Scripts.Player;
+using UnityEngine;
+
+namespace LJH.Scripts.Map
+{
+    public class Well : TriggerBase
+    {
+        protected override void HumanEnter(Collider2D thePlayer)
+        {
+            GameManager.Instance.CmdGameOver("盲人落入井中");
+            thePlayer.GetComponent<PlayerHuman>().CanMove = false;
+            thePlayer.GetComponent<PlayerHuman>().TheAnimator.SetTrigger("FallWater");
+        }
+
+        protected override void HumanExit(Collider2D thePlayer)
+        {
+            
+        }
+
+        protected override void DogEnter(Collider2D thePlayer)
+        {
+            GameManager.Instance.CmdGameOver("导盲犬落入井中");
+            thePlayer.GetComponent<PlayerDog>().CanMove = false;
+            thePlayer.GetComponent<PlayerDog>().TheAnimator.SetTrigger("FallWater");
+        }
+
+        protected override void DogExit(Collider2D thePlayer)
+        {
+            
+        }
+    }
+}
--- Assets/LJH/Scripts/PedestrianCreator.cs
+++ Assets/LJH/Scripts/PedestrianCreator.cs
@@ -1,11 +1,43 @@
-﻿using UnityEngine;
+﻿using Mirror;
+using PurpleFlowerCore;
+using UnityEngine;
 
 namespace LJH.Scripts
 {
-    public class PedestrianCreator : MonoBehaviour
+    public class PedestrianCreator : NetworkBehaviour
     {
         [SerializeField] private GameObject thePedestrian;
+
+        private void OnEnable()
+        {
+            EventSystem.AddEventListener("GameStart",CmdCreatePedestrian);
+        }
         
+        private void OnDisable()
+        {
+            EventSystem.RemoveEventListener("GameStart",CmdCreatePedestrian);
+        }
+
+        // public override void OnStartClient()
+        // {
+        //     base.OnStartClient();
+        //     CmdCreateCar();
+        // }
+
+        // [ClientRpc]
+        // private void RpcCreatePedestrian()
+        // {
+        //     
+        // }
+        
+        [Command(requiresAuthority = false)]
+        private void CmdCreatePedestrian()
+        {
+            //RpcCreatePedestrian();
+            var pedestrian = Instantiate(thePedestrian, transform.position, Quaternion.identity);
+            //var theCar = PoolSystem.GetGameObject(car);
+            NetworkServer.Spawn(pedestrian);
+        }
         
     }
 }
